4e6bdb460b50cf7beb5907d73c7868c070f1fb35
HeitorBraga <heitorbrasil62@gmail.com>
2025-02-20 18:05:26 -0300

    create registration path

--- app/controllers/product_costs_controller.rb
+++ app/controllers/product_costs_controller.rb
@@ -2,6 +2,8 @@ class ProductCostsController < ApplicationController
   include Controllers::Crudify
   model_klass ProductCost
 
+  before_action :check_registration_path, only: %i(new index)
+
   def create
     ProductCost.new(
       product_id: params['product_cost']['product_id'],
@@ -77,6 +79,12 @@ class ProductCostsController < ApplicationController
   end
 
   private
+
+  def check_registration_path
+    unless Product.count > 0
+      redirect_to new_product_path, notice: 'Cadastre um Produto primeiro!'
+    end
+  end
 
   # Only allow a trusted parameter "white list" through.
   def resource_params
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,13 @@
 class ProductsController < ApplicationController
   include Controllers::Crudify
   model_klass Product
+  before_action :check_registration_path, only: %i(index new)
+
+  def check_registration_path
+    unless ProductCategory.count > 0
+      redirect_to new_product_category_path, notice: 'Cadastre uma categoria primeiro!'
+    end
+  end
 
   private
 
